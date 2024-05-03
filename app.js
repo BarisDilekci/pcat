ad8618735251443b2059da8723b9cf909e80ec10
Barış Dilekçi <barisdilekci@Bars-MacBook-Air.local>
2024-05-04 01:24:53 +0300

    feat: middleware tuning done

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const fn = function () {
-  console.log("hello world");
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Statik dosyaların bulunduğu dizini belirtin
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Önceki middleware fonksiyonunu tanımlayın
+const myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
 };
+
+// Middleware fonksiyonunu uygulayın
+app.use(myLogger);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'template', 'index.html')); // Dosya yolu doğru bir şekilde belirtilmeli
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
