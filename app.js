1c317d36820c0f1540f7237e31ebd49351a56584
BarisDilekci <bdilekci200@gmail.com>
2024-05-07 21:56:39 +0300

    feat: photos request body

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const path = require('path');
 
 const app = express();
 
-
-//TEMPLATE ENGINE 
+// TEMPLATE ENGINE 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const myLogger = function (req, res, next) {
   console.log('LOGGED');
   next();
 };
 
+// Middleware'leri myLogger'dan sonra kullanmalısınız, böylece istek gövdesini işleyebilirsiniz.
 app.use(myLogger);
 
 app.get('/', (req, res) => {
@@ -27,6 +29,10 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
