2045384bc8c969c72eb09d4473aa9d7428007fca
BarisDilekci <bdilekci200@gmail.com>
2024-05-09 22:54:19 +0300

    feat: image upload completed

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const ejs = require('ejs');
 const path = require('path');
 const Photo = require('./models/Photo');
 const mongoose = require('mongoose')
- // Örnek bir dosya yolu, bu dosya yolunu kendi proje yapılandırmanıza göre ayarlayın.
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 
 const app = express();
 
 // TEMPLATE ENGINE 
 app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://localhost/pcat-test-db')
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 const myLogger = function (req, res, next) {
   console.log('LOGGED');
@@ -22,6 +24,8 @@ const myLogger = function (req, res, next) {
 };
 
 app.use(myLogger);
+
+
 
 
   app.get('/', async (req, res) => {
@@ -49,10 +53,33 @@ app.get('/add', (req, res) => {
 app.get('/photo', (req, res) => {
   res.render('photo');
 });
+
+
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  //console.log(req.files.image);
+  //res.redirect('/')
+
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) { // Bunun için const fs = require('fs'); almamız gerekir.
+    fs.mkdirSync(uploadDir);
+  }
+  
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, 
+    async () => {
+      await Photo.create({
+        ...req.body ,
+        image: '/uploads/' + uploadeImage.name,
+      });
+      res.redirect('/');
+    }
+  )
 });
+
 
 const port = 3000;
 app.listen(port, () => {
