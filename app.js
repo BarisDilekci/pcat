6ab3d04bc8bfec6f042ed4d74a99d8c35707f678
BarisDilekci <bdilekci200@gmail.com>
2024-05-11 01:25:19 +0300

    feat: update photo detail

--- app.js
+++ app.js
@@ -1,85 +1,91 @@
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
-const mongoose = require('mongoose')
-const fileUpload = require('express-fileupload');
-const fs = require('fs');
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const Photo = require("./models/Photo");
+const mongoose = require("mongoose");
+const methodOverride = require('method-override')
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
 
 const app = express();
 
-// TEMPLATE ENGINE 
+// TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://localhost/pcat-test-db');
+mongoose.connect("mongodb://localhost/pcat-test-db");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 const myLogger = function (req, res, next) {
-  console.log('LOGGED');
+  console.log("LOGGED");
   next();
 };
 
 app.use(myLogger);
 
-
-
-
-  app.get('/', async (req, res) => {
-    const photos = await Photo.find({})
-    res.render('index',{
-      photos
-    });
-
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
   });
+});
 
-app.get('/photos/:id', async (req,res) => {
+app.get("/photos/:id", async (req, res) => {
   const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo
+  res.render("photo", {
+    photo,
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/add', (req, res) => {
-  res.render('add');
+app.get("/add", (req, res) => {
+  res.render("add");
 });
-app.get('/photo', (req, res) => {
-  res.render('photo');
+app.get("/photo", (req, res) => {
+  res.render("photo");
 });
 
+app.post("/photos", async (req, res) => {
+  const uploadDir = "public/uploads";
 
-app.post('/photos', async (req, res) => {
-  //console.log(req.files.image);
-  //res.redirect('/')
-
-
-  const uploadDir = 'public/uploads';
-
-  if (!fs.existsSync(uploadDir)) { // Bunun için const fs = require('fs'); almamız gerekir.
+  if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
-  
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
 
-  uploadeImage.mv(uploadPath, 
-    async () => {
-      await Photo.create({
-        ...req.body ,
-        image: '/uploads/' + uploadeImage.name,
-      });
-      res.redirect('/');
-    }
-  )
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadeImage.name,
+    });
+    res.redirect("/");
+  });
 });
 
+app.get('/photos/edit/:id', async (req,res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit' ,{
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
+});
 
 const port = 3000;
 app.listen(port, () => {
