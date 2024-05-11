d4b0c24b5feb46ff968278b7465465c8e4e21f1e
BarisDilekci <bdilekci200@gmail.com>
2024-05-12 02:30:21 +0300

    feat: MVC Refactoring

--- app.js
+++ app.js
@@ -1,105 +1,53 @@
 const express = require("express");
 const ejs = require("ejs");
-const path = require("path");
-const Photo = require("./models/Photo");
-const mongoose = require("mongoose");
-const methodOverride = require('method-override')
 const fileUpload = require("express-fileupload");
-const fs = require("fs");
-
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const photoController = require("./controllers/photoControllers");
+const pageController = require("./controllers/pageControllers");
+const Photo = require('./models/Photo');
 const app = express();
+const fs = require("fs"); 
 
-// TEMPLATE ENGINE
+
+//CONNECT DB
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost/pcat-test-db",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+connectDb();
+
+//TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost/pcat-test-db");
-
-app.use(express.static(path.join(__dirname, "public")));
+//MIDDLEWARES
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method', {
-methods: ['POST', 'GET'],
-}));
+app.use(methodOverride("_method", { methods: ["GET", "POST"] }));
 
+//ROUTES
+app.get("/", photoController.getAllPhotos);
+app.get("/photos/:id", photoController.getPhoto);
+app.post("/photos", photoController.createPhoto);
+app.put("/photos/:id", photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto);
 
-const myLogger = function (req, res, next) {
-  console.log("LOGGED");
-  next();
-};
+app.get("/about", pageController.getAboutPage);
+app.get("/add", pageController.getAddPage);
+app.get("/edit/:id", pageController.getEditPage);
 
-app.use(myLogger);
-
-app.get("/", async (req, res) => {
-  const photos = await Photo.find({});
-  res.render("index", {
-    photos,
-  });
-});
-
-app.get("/photos/:id", async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render("photo", {
-    photo,
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add");
-});
-app.get("/photo", (req, res) => {
-  res.render("photo");
-});
-
-app.post("/photos", async (req, res) => {
-  const uploadDir = "public/uploads";
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
-
-  uploadeImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: "/uploads/" + uploadeImage.name,
-    });
-    res.redirect("/");
-  });
-});
-
-app.get('/photos/edit/:id', async (req,res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render('edit' ,{
-    photo,
-  });
-});
-
-app.put('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title
-  photo.description = req.body.description
-  photo.save()
-
-  res.redirect(`/photos/${req.params.id}`)
-});
-
-app.delete('/photos/:id', async (req, res) => {
-  console.log(req.params.id);
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deleteImage = __dirname  + '/public' + photo.image;
-  if(fs.existsSync(deleteImage)){ fs.unlinkSync(deleteImage) }
-  await Photo.findByIdAndDelete(req.params.id);
-  res.redirect('/');
-});
-
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+  console.log("Project is up");
 });
